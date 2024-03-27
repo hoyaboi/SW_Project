78740ca5fc3a2dca6b664c07d395ed7fbf1eac7a
sun <sunminjun89@gmail.com>
2024-03-28 02:27:36 +0900

    3/28_2

--- app/src/main/java/com/example/sw_project/LoginActivity.kt
+++ app/src/main/java/com/example/sw_project/LoginActivity.kt
@@ -1,20 +1,70 @@
 package com.example.sw_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        auth = FirebaseAuth.getInstance()
+
+        val signInButton: Button = findViewById(R.id.signin_button)
+        val signUpButton: Button = findViewById(R.id.signup_button)
+        signInButton.setOnClickListener {
+            signIn()
+        }
+        signUpButton.setOnClickListener {
+            signUp()
+        }
+
+    }
+    private fun signIn() {
+        val idEditText: TextInputEditText = findViewById(R.id.id_edittext)
+        val passwordEditText: TextInputEditText = findViewById(R.id.pwd_edittext)
+        val id = idEditText.text.toString().trim()
+        val pwd = passwordEditText.text.toString().trim()
+
+        if(id.isNotEmpty() && pwd.isNotEmpty()) {
+            auth.signInWithEmailAndPassword(id, pwd).addOnCompleteListener(this) { task ->
+                if(task.isSuccessful) {
+                    moveStartPage(task.result.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+    private fun signUp() {
+        val idEditText: TextInputEditText = findViewById(R.id.id_edittext)
+        val passwordEditText: TextInputEditText = findViewById(R.id.pwd_edittext)
+        val id = idEditText.text.toString().trim()
+        val pwd = passwordEditText.text.toString().trim()
+
+        if(id.isNotEmpty() && pwd.isNotEmpty()) {
+            auth.createUserWithEmailAndPassword(id, pwd).addOnCompleteListener(this) { task ->
+                if(task.isSuccessful) {
+                    moveStartPage(task.result.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+    private fun moveStartPage(user:FirebaseUser?) {
+        if(user != null) {
+            startActivity(Intent(this, StartActivity::class.java))
         }
     }
 }
