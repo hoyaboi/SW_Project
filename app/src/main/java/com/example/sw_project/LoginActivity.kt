4907a88c8b7348de23b47e0c88d8a24b273ada03
sun <sunminjun89@gmail.com>
2024-04-04 01:49:49 +0900

    4/4

--- app/src/main/java/com/example/sw_project/LoginActivity.kt
+++ app/src/main/java/com/example/sw_project/LoginActivity.kt
@@ -1,20 +1,26 @@
 package com.example.sw_project
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -22,14 +28,55 @@ class LoginActivity : AppCompatActivity() {
 
         val signInButton: Button = findViewById(R.id.signin_button)
         val signUpButton: Button = findViewById(R.id.signup_button)
+        val signUpGoogleButton : Button = findViewById(R.id.signin_google)
+
         signInButton.setOnClickListener {
             signIn()
         }
         signUpButton.setOnClickListener {
-            signUp()
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+        signUpGoogleButton.setOnClickListener {
+            googleLogin()
         }
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("1054487677216-u6f28mqmd1n7f3k96rr4inl0stv0qahu.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
+
+    // google 로그인
+    private fun googleLogin() {
+        val signInIntent = googleSignInClient.signInIntent
+        googleLoginLauncher.launch(signInIntent)
+    }
+    private val googleLoginLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if(result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try { // Google 로그인 성공, Firebase로 인증 정보 교환
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) { // Google 로그인 실패 처리
+                Log.w("GoogleLogin", "Google sign in failed", e)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener(this) { task ->
+            if(task.isSuccessful) {
+                startActivity(Intent(this, SignupGoogleActivity::class.java))
+            } else {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    // 이메일 로그인 및 회원가입
     private fun signIn() {
         val idEditText: TextInputEditText = findViewById(R.id.id_edittext)
         val passwordEditText: TextInputEditText = findViewById(R.id.pwd_edittext)
@@ -55,7 +102,7 @@ class LoginActivity : AppCompatActivity() {
         if(id.isNotEmpty() && pwd.isNotEmpty()) {
             auth.createUserWithEmailAndPassword(id, pwd).addOnCompleteListener(this) { task ->
                 if(task.isSuccessful) {
-                    moveStartPage(task.result.user)
+                    startActivity(Intent(this, LoginActivity::class.java))
                 } else {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
@@ -67,4 +114,5 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, StartActivity::class.java))
         }
     }
+
 }
