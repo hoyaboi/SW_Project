70670a7250b6182f03e5590638bae48886b55ab2
Soohyun Park <pshyun03@gmail.com>
2024-05-12 17:12:17 +0900

    5/12_album_create

--- app/src/main/java/com/example/sw_project/tabbar/AlbumFragment.kt
+++ app/src/main/java/com/example/sw_project/tabbar/AlbumFragment.kt
@@ -8,12 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sw_project.AddAlbumActivity
+import com.example.sw_project.AddBoardActivity
 import com.example.sw_project.Album_Adapter
 import com.example.sw_project.Album_list
-import com.example.sw_project.PhotoActivity
-import com.example.sw_project.R
+import com.example.sw_project.PhotoviewActivity
 import com.example.sw_project.databinding.FragmentAlbumBinding
-//start액티비티와 같은 원리로 앨범 목록 출력
+
+
 class AlbumFragment : Fragment() {
     private var userEmail: String? = null
     private var roomID: Int = 0
@@ -24,6 +26,7 @@ class AlbumFragment : Fragment() {
     private lateinit var binding2:FragmentAlbumBinding
     val Albumlist= arrayListOf<Album_list>()
     val Albumadapter=Album_Adapter(Albumlist)
+
     lateinit var Albumname:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,30 +35,6 @@ class AlbumFragment : Fragment() {
             userEmail = it.getString("userEmail")
             roomID = it.getInt("roomID", 0)
         }
-        binding2=FragmentAlbumBinding.inflate(layoutInflater)
-        val view=binding2.root
-
-        
-        //setContentView(view)
-
-        binding2.recyclerview.layoutManager=LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        binding2.recyclerview.adapter=Albumadapter
-
-        //Albumadapter.setItemClickListener(object:Album_Adapter.OnItemClickListener{
-          //  override fun onClick(v:View,position:Int){
-            //    val intent=Intent(getActivity(), PhotoActivity::class.java)
-              //  intent.putExtra("albumID",position)
-                //startActivity(intent)
-            //}
-        //})
-
-        Albumlist.add(Album_list("강원도여행"))
-        Albumlist.add(Album_list("여름휴가"))
-        //val name: String=intent.getStringExtra("albumname").toString()
-        //val check=intent.getIntExtra("check",0)
-        //if(check==1){
-        //    Albumlist.add(Album_list(name))
-        //}
     }
 
     override fun onCreateView(
@@ -65,8 +44,41 @@ class AlbumFragment : Fragment() {
         _binding = FragmentAlbumBinding.inflate(inflater, container, false)
         // 여기서 userEmail과 roomID를 사용하여 UI 업데이트
         // user email과 room id 로깅
+        //binding2=FragmentAlbumBinding.inflate(layoutInflater)
+        val view=binding.root
+
+
+        //setContentView(view)
+
+        binding.recyclerview.layoutManager=LinearLayoutManager(this.activity,LinearLayoutManager.HORIZONTAL,false)
+        binding.recyclerview.adapter=Albumadapter
+//앨범 클릭시 앨범 index를 넘겨주며 사진 목록 화면으로 넘어감
+        Albumadapter.setItemClickListener(object:Album_Adapter.OnItemClickListener{
+          override fun onClick(v:View,position:Int){
+            val intent=Intent(activity, PhotoviewActivity::class.java)
+          intent.putExtra("albumID",position)
+        startActivity(intent)
+        }
+        })
+        //테스트
+        Albumlist.add(Album_list("강원도여행"))
+        Albumlist.add(Album_list("여름휴가"))
+        //val name: String=intent.getStringExtra("albumname").toString()
+        //val check=intent.getIntExtra("check",0)
+        //if(check==1){
+        //    Albumlist.add(Album_list(name))
+        //}
+        _binding?.addAlbum?.setOnClickListener {
+            val intent = Intent(activity, AddAlbumActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
         Log.d("AlbumFragment", "user email: ${userEmail}, room ID: $roomID")
         return binding.root
+        //return View(requireActivity())
     }
     override fun onDestroyView() {
         _binding = null
